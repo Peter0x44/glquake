28fa1116c4b02dbb035ded105cbf68df3c804979
Peter0x44 <peter0x44@disroot.org>
2024-06-18 11:27:06 +0100

    Fix -Wimplicit-function-declaration warning
    /__w/glquake/glquake/source/net_udpctr.c:373:28: error: implicit declaration of function 'inet_addr' [-Wimplicit-function-declaration]
      373 |         if (a == 0 || a == inet_addr("127.0.0.1"))
          |                            ^~~~~~~~~

--- source/net_udpctr.c
+++ source/net_udpctr.c
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <malloc.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
 
